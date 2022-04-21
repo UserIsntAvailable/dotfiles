dd9923b40eecc7d6a7b70fcbcb190078b7c63af3
UserIsntAvailable <pinedap.angel@gmail.com>
2022-04-21 17:21:13 -0400

    nvim: migrate from vim.cmd autocommands to new lua autocommand api
    added augroup for checktime autocommand.
    
    lua was added to the remove trailling white spaces
    autocommand pattern filetypes.
    
    fix txt pattern. ( The actual <afile> name is text )

--- .config/nvim/lua/user/autocmds.lua
+++ .config/nvim/lua/user/autocmds.lua
@@ -1,13 +1,44 @@
 
 vim.api.nvim_create_autocmd(
     {"FocusGained", "BufWinEnter"},
-    {command = "checktime"}
+    {
+        command = "checktime",
+        group = vim.api.nvim_create_augroup("CheckChangeOutsideBuffer", {clear = true}),
+        desc = "Checks if any buffer was updated outside nvim"
+    }
 )
 
--- TODO: Convert to nvim autocmd api ( I dont have any idea how to properly do it )
--- Delete trailing white space on save.
--- Return to last edit position when opening files
-vim.cmd [[
-    autocmd FileType txt,c,cpp,cs,java,lua,py autocmd BufWritePre <buffer> %s/\s\+$//e
-    autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-]]
+local rem_trailling_spaces_grp = vim.api.nvim_create_augroup("RemoveTraillingSpaces", {clear = true})
+vim.api.nvim_create_autocmd(
+    "FileType",
+    {
+        pattern = {"text", "c", "cpp", "cs", "java", "python", "lua"},
+        callback = function() -- Im not really sure if this is the way to do it
+            vim.api.nvim_create_autocmd(
+                "BufWritePre",
+                {
+                    pattern = "<buffer>",
+                    callback = function() vim.api.nvim_command([[%s/\s\+$//e]]) end,
+                    group = rem_trailling_spaces_grp,
+                    desc = "Delete trailing white spaces on save"
+                }
+            )
+        end,
+        group = rem_trailling_spaces_grp,
+        desc = "Checks if filetype if valid for trailling white spaces removal"
+    }
+)
+
+vim.api.nvim_create_autocmd(
+    "BufReadPost",
+    {
+        callback = function()
+            local line = vim.fn.line
+            if line([['"]]) > 1 and line([['"]]) <= line("$") then
+                vim.api.nvim_command([[normal! g'"]])
+            end
+        end,
+        group = vim.api.nvim_create_augroup("ReturnToLastCursorPos", {clear = true}),
+        desc = "Return to last edit position when opening files",
+    }
+)
