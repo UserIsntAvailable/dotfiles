a3a8abc1aa51e6058fc83f1a329d2eeb1666ed85
UserIsntAvailable <pinedap.angel@gmail.com>
2022-04-22 11:58:41 -0400

    nvim: tweat colorscheme colors to match my vimrc
    remove search, and spell related highlights.
    ( They look better in neovim for some reason )
    
    remove sh highlight. I will use later treesitter to
    highlight specific languages syntax.

--- .config/nvim/lua/config/colorscheme.lua
+++ .config/nvim/lua/config/colorscheme.lua
@@ -1,2 +1,5 @@
 
-vim.cmd "colorscheme jellybeans-nvim" -- TODO: Tweat colors to match my vimrc theming
+vim.cmd "colorscheme jellybeans-nvim"
+
+vim.api.nvim_set_hl(0, "CursorLineNr", { bold = true })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#303030", bold = true })
