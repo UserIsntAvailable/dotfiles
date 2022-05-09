cc41ef379675b8223def1973038e97c9f8161d1d
UserIsntAvailable <pinedap.angel@gmail.com>
2022-05-09 08:55:45 -0400

    Revert "bufferline: use default close_command"
    This reverts commit ea3be653fb095ffe4fdc2bcbcd6a6937fab392ce.

--- .config/nvim/lua/config/plugins/bufferline.lua
+++ .config/nvim/lua/config/plugins/bufferline.lua
@@ -10,11 +10,9 @@ end
 
 -- utils --
 
--- FIX: I think that NvimTree and bufferline do not like each other
-
--- local function buf_close_handler(bufnum)
--- bufdelete.bufdelete(bufnum, true)
--- end
+local function buf_close_handler(bufnum)
+    bufdelete.bufdelete(bufnum, true)
+end
 
 local function remove_buf_extension(buf)
     return vim.fn.fnamemodify(buf.name, ':t:r')
@@ -28,7 +26,7 @@ end
 
 bufferline.setup {
     options = {
-        -- close_command = buf_close_handler,
+        close_command = buf_close_handler,
         diagnostics = "nvim_lsp",
         diagnostics_indicator = format_lsp_indications,
         diagnostics_update_in_insert = false,
@@ -43,11 +41,10 @@ bufferline.setup {
         show_close_icon = false,
         sort_by = "insert_at_end",
         tab_size = 1,
-        offsets = { { filetype = "NvimTree", text = "NvimTree", text_align = "center" } },
+        offsets = {{filetype = "NvimTree", text = "NvimTree", text_align = "center"}},
         groups = { items = { require("bufferline.groups").builtin.pinned:with({ icon = "" }), } }
     },
 }
 
--- require("config.keymaps").bufdelete(bufdelete) -- I will keep this here, since creating a file for only keymaps would be silly.
-vim.keymap.set("n", "<Leader>bdd", ":bdelete %<CR>", { silent = true, desc = "Close a buffer" }) -- Keeping this for now
+require("config.keymaps").bufdelete(bufdelete) -- I will keep this here, since creating a file for only keymaps would be silly.
 require("config.keymaps").bufferline()
