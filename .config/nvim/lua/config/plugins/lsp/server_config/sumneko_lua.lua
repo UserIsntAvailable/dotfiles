033ecc63b51fab93a18d5ab2d6a6312513adcb75
UserIsntAvailable <pinedap.angel@gmail.com>
2022-05-16 17:44:43 +0000

    sumneko_lua: change `root_dir` and `workspace.library` dinamically
    plugins/init.lua: remove `require_plugin`
    ( now I can use go_to_definition )

--- .config/nvim/lua/config/plugins/lsp/server_config/sumneko_lua.lua
+++ .config/nvim/lua/config/plugins/lsp/server_config/sumneko_lua.lua
@@ -1,21 +1,67 @@
---[[
-    TODO: Actually understand what this is doing.
-    The config example from here: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-    is not working correctly, but this https://github.com/LunarVim/Neovim-from-scratch/blob/06-LSP/lua/user/lsp/settings/sumneko_lua.lua
-    is fine, so I will be using that for the moment.
---]]
+local lspc_util = require("lspconfig.util")
+
+-- FIX: The sumneko_lua files are also being loaded
+
+local lua_projects = {
+    {
+        name = "nvim",
+        library = vim.api.nvim_get_runtime_file("", true),
+    },
+    -- TODO: Find runtime file path
+    {
+        name = "awesome",
+    },
+    {
+        name = "wezterm",
+    },
+}
+
+local current_project
+
+local function run_on_project_mode(startpath)
+    local config_path = vim.fn.expand("$XDG_CONFIG_HOME/")
+
+    local project_paths = vim.tbl_map(function(project)
+        return config_path .. project.name
+    end, lua_projects)
+
+    local function match(path)
+        for _, project in ipairs(project_paths) do
+            if project == path then
+                current_project = project
+                return path
+            end
+        end
+    end
+
+    return lspc_util.search_ancestors(startpath, match)
+end
+
+local function get_project_library(project_path)
+    if not project_path then
+        return
+    end
+
+    local name = vim.fn.fnamemodify(project_path, ":t")
+    for _, table in ipairs(lua_projects) do
+        if table.name == name then
+            return table.library
+        end
+    end
+end
+
 return {
+    root_dir = run_on_project_mode,
     settings = {
         Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true,
-                },
-            },
+            diagnostics = { globals = { "vim" } },
         },
     },
+    on_init = function(client)
+        client.config.settings.Lua.workspace = {
+            library = get_project_library(current_project),
+        }
+        client.notify("workspace/didChangeConfiguration")
+        return true
+    end,
 }
