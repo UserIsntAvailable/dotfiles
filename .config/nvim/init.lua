a1ac67f47bccb7865a3ee2d012968ebd9ad78433
UserIsntAvailable <pinedap.angel@gmail.com>
2022-09-04 16:01:14 -0400

    add `neovide` specific config files.
    Added a new `external` module, which holds config files for external
    applications that use/require neovim.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,13 @@
+
+-- Required
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 require("config.statusline")
 require("config.colorscheme")
+
+-- Everything after this should be wrapped inside pcallrequireq ( or similar
+-- mechanisms ), in case something is missing.
+
 require("config.plugins")
+require("config.external") -- External software
