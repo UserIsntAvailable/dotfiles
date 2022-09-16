52914a6a65ac1931334c13a6b1bc61bfd8ca19a3
UserIsntAvailable <pinedap.angel@gmail.com>
2022-09-16 16:56:57 -0400

    lsp: Add `fsharp` language support.
    Changelog:
    - Add `snippet` capability
    - Fix error where you were not able to override the `default_server_setup`
    options (I was using tbl_deep_extend "force" instead of "keep").

--- .config/nvim/lua/config/plugins/lsp/config.lua
+++ .config/nvim/lua/config/plugins/lsp/config.lua
@@ -3,22 +3,25 @@ if not lspconfig_ok then
     return
 end
 
--- optional lsp extensions --
+-- optional --
 
-local cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-local lsp_inst_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+local cmp_ok, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+local lsp_installer_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+local luasnip_ok = pcall(require, "luasnip")
 
 -- server config --
 
 local servers = { -- TODO: Maybe I should put all server config files here?
-    "sumneko_lua",
     "csharp_ls",
+    "cssls",
+    "fsautocomplete",
+    -- "omnisharp",
+    "sumneko_lua",
     "tsserver",
-    "cssls"
 }
 
-if lsp_inst_ok then
-    -- All the available language servers: https://github.com/williamboman/nvim-lsp-installer#available-lsps
+if lsp_installer_ok then
+    -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
     lsp_installer.setup({ ensure_installed = servers })
 end
 
@@ -28,7 +31,10 @@ local default_server_setup = {
     capabilities = (function()
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         if cmp_ok then
-            capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+            capabilities = cmp_lsp.update_capabilities(capabilities)
+        end
+        if luasnip_ok then
+            capabilities.textDocument.completion.completionItem.snippetSupport = true
         end
         return capabilities
     end)(),
@@ -41,7 +47,7 @@ for _, server in pairs(servers) do
     local configured, server_opts = pcall(require, "config.plugins.lsp.server_config." .. server)
 
     if configured then
-        opts = vim.tbl_deep_extend("force", server_opts, opts)
+        opts = vim.tbl_deep_extend("keep", server_opts, opts)
     end
 
     lspconfig[server].setup(opts)
