306d3142dda8f30361dd34440803dc62856f1d58
UserIsntAvailable <pinedap.angel@gmail.com>
2022-10-03 18:31:52 -0400

    lsp: Add `shellcheck` `code_actions` and `diagnostics`.
    Changelog:
    - Add `max-line-length` to `pylint` source.
    - Some WIP stuff of `mypy` and `pylint` ( to lazy to remove. My `null-ls`
      PR should be done this week ).

--- .config/nvim/lua/config/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/config/plugins/lsp/null-ls.lua
@@ -6,14 +6,53 @@ end
 local null_ls_extra_folder_path = vim.fn.stdpath("config") .. "/extra/null-ls/"
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
+
+-- TODO: Instead of having lsp configuration and null-ls configuration I should
+-- probably create lsp configuration per language. So all configuration of
+-- python ( pylsp, flake8, mypy, pylink, and black ) should be at python.lua
+
+local MAX_LINE_LENGTH = 100
+-- local VIRTUAL_ENV = "$VIRTUAL_ENV"
+-- local VENV_SITE_PACKAGES_PATH = (function() -- FIX: There is probably an easier way to do this.
+--     local venv = vim.fn.expand(VIRTUAL_ENV)
+--
+--     if venv == VIRTUAL_ENV then
+--         -- TODO: Automatically activate the virtual env.
+--         return vim.schedule(function()
+--             vim.notify("Don't forget to enter the virtual environment.", vim.log.levels.ERROR)
+--         end)
+--     end
+--
+--     local python_path = require("plenary.scandir").scan_dir(
+--         venv .. "/lib",
+--         { depth = 1, add_dirs = true, only_dirs = true }
+--     )[1]
+--
+--     return python_path .. "/site-packages"
+-- end)() or ""
+
 local sources = {
+    code_actions = {
+        shellcheck = {},
+    },
     diagnostics = {
-        flake8 = { extra_args = { "--max-line-length", 100 } },
-        mypy = {},
-        pylint = {},
+        flake8 = { extra_args = { "--max-line-length", MAX_LINE_LENGTH } },
+        mypy = {
+            env = nil,--[[ { MYPYPATH = VENV_SITE_PACKAGES_PATH } ]]
+        },
+        pylint = {
+            extra_args = { "--max-line-length", MAX_LINE_LENGTH, "--disable", "C0111" },
+            -- env = {
+            --     -- stylua: ignore
+            --     PYTHONPATH = VENV_SITE_PACKAGES_PATH
+            --         .. (jit.os == "Windows" and ";" or ":")
+            --         .. vim.fn.fnamemodify(VIRTUAL_ENV, ":p:h") .. "/src",
+            -- },
+        },
+        shellcheck = {},
     },
     formatting = {
-        black = { extra_args = { "--preview", "-l", 100 } },
+        black = { extra_args = { "--preview", "-l", MAX_LINE_LENGTH } },
         shfmt = { extra_args = { "-i", 4, "-ci", "-sr", "-fn" } },
         stylua = {
             extra_args = function(params)
@@ -44,11 +83,11 @@ for source_section, source_section_table in pairs(sources) do
                 sources_modules,
                 null_ls.builtins[source_section][source].with(source_table)
             )
-        else
-            -- Open `null-ls` checkhealth in another buffer.
         end
     end
 end
+
+-- TODO: Open `null-ls` checkhealth in another buffer. If any source is missing.
 
 null_ls.setup({
     default_timeout = 2000, -- I will tweeak by source/project if needed.
