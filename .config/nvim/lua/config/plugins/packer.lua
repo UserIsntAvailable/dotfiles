67dd903ecbef7f31777d91a697a2be911efe25c0
UserIsntAvailable <pinedap.angel@gmail.com>
2022-11-22 03:56:52 -0500

    fix(packer): use plenary github fork, instead of local repo.

--- .config/nvim/lua/config/plugins/packer.lua
+++ .config/nvim/lua/config/plugins/packer.lua
@@ -34,7 +34,7 @@ return packer.startup(function(use)
     use "wbthomason/packer.nvim"                        -- Have packer manage itself
     use "nvim-lua/popup.nvim"                           -- An implementation of the Popup API from vim in Neovim
     -- use "nvim-lua/plenary.nvim"                      -- Useful lua functions used by lots of plugins
-    use "~/.local/repos/lua/plenary.nvim"
+    use { "UserIsntAvailable/plenary.nvim", branch = "memoize-function" }
     use "kyazdani42/nvim-web-devicons"                  -- API to work with icons easier
     use "UserIsntAvailable/dotscode"
 
