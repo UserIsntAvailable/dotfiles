7f4969750db21dcef28800e92d66cf593e54bf2c
UserIsntAvailable <pinedap.angel@gmail.com>
2022-11-22 02:32:25 -0500

    revert: 'feat(lsp): add lsp_signature.nvim.'

--- .config/nvim/lua/config/plugins/lsp/handlers.lua
+++ .config/nvim/lua/config/plugins/lsp/handlers.lua
@@ -3,16 +3,12 @@ local M = {}
 -- TODO: Configure diagnostics.
 -- TODO: Configure codelens.
 
-M.on_attach = function(client, bufnr)
+M.on_attach = function(client, _)
     require("config.keymaps").lsp_buffer()
 
     if client.server_capabilities.documentHighlightProvider then
         require("config.autocmds").lsp_document_highlight()
     end
-
-    -- if client.server_capabilities.signatureHelpProvider then
-    --     require("lsp_signature").attach(client, bufnr)
-    -- end
 end
 
 return M
